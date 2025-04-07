e5ebfa31b79b4accfa35d01355898abf3afb3852
Felipe Gallego <felipegallego600@gmail.com>
2025-04-06 21:39:45 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecoverComponent } from './pages/recover/recover.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'recover', component: RecoverComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'user', component: UserComponent },
+  { path: '**', redirectTo: 'landing' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
